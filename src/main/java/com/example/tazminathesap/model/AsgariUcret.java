b23c994c05404b870b8422cadd4a917de68d9798
Alpin Alperen Aşkın <alpin_askin@hotmail.com>
2022-03-09 15:18:49 +0300

    AsgariUcret Entity Edited

--- src/main/java/com/example/tazminathesap/model/AsgariUcret.java
+++ src/main/java/com/example/tazminathesap/model/AsgariUcret.java
@@ -10,5 +10,47 @@ public class AsgariUcret extends BaseEntity {
 	private Date baslangicTarih;
 	private Date bitisTarih;
 	private Double asgariUcretMiktar;
+
+	public AsgariUcret(Date baslangicTarih, Date bitisTarih, Double asgariUcretMiktar) {
+		this.baslangicTarih = baslangicTarih;
+		this.bitisTarih = bitisTarih;
+		this.asgariUcretMiktar = asgariUcretMiktar;
+	}
+
+	public AsgariUcret() {
+	}
 	
+
+	public Date getBaslangicTarih() {
+		return this.baslangicTarih;
+	}
+
+	public void setBaslangicTarih(Date baslangicTarih) {
+		this.baslangicTarih = baslangicTarih;
+	}
+
+	public Date getBitisTarih() {
+		return this.bitisTarih;
+	}
+
+	public void setBitisTarih(Date bitisTarih) {
+		this.bitisTarih = bitisTarih;
+	}
+
+	public Double getAsgariUcretMiktar() {
+		return this.asgariUcretMiktar;
+	}
+
+	public void setAsgariUcretMiktar(Double asgariUcretMiktar) {
+		this.asgariUcretMiktar = asgariUcretMiktar;
+	}
+
+	@Override
+	public String toString() {
+		return "{" +
+			" baslangicTarih='" + getBaslangicTarih() + "'" +
+			", bitisTarih='" + getBitisTarih() + "'" +
+			", asgariUcretMiktar='" + getAsgariUcretMiktar() + "'" +
+			"}";
+	}
 }
