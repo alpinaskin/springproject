3fe26015ff17bddbfcefba49092d2a86b7a4d47e
Alpin Alperen Aşkın <alpin_askin@hotmail.com>
2022-03-10 14:44:26 +0300

    AsgariUcret Repository Created

---
+++ src/main/java/com/example/tazminathesap/repository/AsgariUcretRepository.java
@@ -0,0 +1,9 @@
+package com.example.tazminathesap.repository;
+
+import com.example.tazminathesap.model.AsgariUcret;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AsgariUcretRepository extends JpaRepository<AsgariUcret, Long> {
+    
+}
