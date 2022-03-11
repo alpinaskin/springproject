e0c074ea78965508e3532ea7267b11c50068cfac
Alpin Alperen Aşkın <alpin_askin@hotmail.com>
2022-03-11 16:03:06 +0300

    GecmisDevreHesabi Service Created

---
+++ src/main/java/com/example/tazminathesap/service/GecmisDevreHesabiService.java
@@ -0,0 +1,7 @@
+package com.example.tazminathesap.service;
+
+import com.example.tazminathesap.model.GecmisDevreHesabi;
+
+public interface GecmisDevreHesabiService extends CrudService<GecmisDevreHesabi> {
+
+}
---
+++ src/main/java/com/example/tazminathesap/service/jpa/GecmisDevreHesabiServiceJPA.java
@@ -0,0 +1,21 @@
+package com.example.tazminathesap.service.jpa;
+
+import com.example.tazminathesap.model.GecmisDevreHesabi;
+import com.example.tazminathesap.repository.GecmisDevreHesabiRepository;
+import com.example.tazminathesap.service.GecmisDevreHesabiService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GecmisDevreHesabiServiceJPA extends AbstractJpaService<GecmisDevreHesabi, GecmisDevreHesabiRepository> implements GecmisDevreHesabiService{
+
+    public GecmisDevreHesabiServiceJPA(GecmisDevreHesabiRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    public GecmisDevreHesabi save(GecmisDevreHesabi gem){
+        return gem;
+    }
+    
+}
