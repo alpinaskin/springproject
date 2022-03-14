eaeb80c09635162f5f1a6e6b3b25d453ba66bf4b
Alpin Alperen Aşkın <alpin_askin@hotmail.com>
2022-03-14 15:32:56 +0300

    ZararDonemi Entity created

---
+++ src/main/java/com/example/tazminathesap/model/ZararDonemi.java
@@ -0,0 +1,34 @@
+package com.example.tazminathesap.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ZararDonemi {
+    String zararDonemi;
+    Double donemZarar;
+
+    public ZararDonemi() {
+    }
+
+    public ZararDonemi(String zararDonemi, Double donemZarar) {
+        this.zararDonemi = zararDonemi;
+        this.donemZarar = donemZarar;
+    }
+
+    public String getZararDonemi() {
+        return this.zararDonemi;
+    }
+
+    public void setZararDonemi(String zararDonemi) {
+        this.zararDonemi = zararDonemi;
+    }
+
+    public Double getDonemZarar() {
+        return this.donemZarar;
+    }
+
+    public void setDonemZarar(Double donemZarar) {
+        this.donemZarar = donemZarar;
+    }
+
+}
