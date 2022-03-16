0e17f85bfd3b721d383865215c3c32ec782aa8e8
Alpin Alperen Aşkın <alpin_askin@hotmail.com>
2022-03-16 09:55:04 +0300

    Lombok ToString stackoverflow err fixed

--- src/main/java/com/example/tazminathesap/model/TazminatRapor.java
+++ src/main/java/com/example/tazminathesap/model/TazminatRapor.java
@@ -10,13 +10,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 @Entity
 public class TazminatRapor extends BaseEntity{
 	@JsonManagedReference
